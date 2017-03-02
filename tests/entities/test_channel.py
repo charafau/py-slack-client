887006e932518bf8f5cedee63081f5fda957b16b
charafau <gorudonu@gmail.com>
2017-03-03 03:35:12 +0900

    Added NamedTuple for channel class

--- tests/entities/test_channel.py
+++ tests/entities/test_channel.py
@@ -1,17 +1,23 @@
 from unittest import TestCase
 
+from app.entities.channel import Channel
+
 
 class TestChannel(TestCase):
     def setUp(self):
-        self.json = """{'id': 'C3WBCUECA', 'name': 'general', 'is_channel': True,
-         'created': 1485362015, 'creator': 'U3WDY4PEH', 'is_archived': False, 'is_general': True,
-          'is_member': True, 'members': ['U3WDY4PEH', 'U3X42THJB'],
-          'topic': {'value': 'Company-wide announcements and work-based matters', 'creator': '', 'last_set': 0},
-          'purpose': {'value': 'This channel is for team-wide communication and announcements.
-          All team members are in this channel.',
-          'creator': '', 'last_set': 0}, 'previous_names': [], 'num_members': 2}"""
+        self.channel_dict = {'id': 'C3WBCUECA', 'name': 'general', 'is_channel': True, 'created': 1485362015,
+                             'creator': 'U3WDY4PEH', 'is_archived': False, 'is_general': True, 'is_member': True,
+                             'members': ['U3WDY4PEH', 'U3X42THJB'],
+                             'topic': {'value': 'Company-wide announcements and work-based matters',
+                                       'creator': '', 'last_set': 0},
+                             'purpose': {
+                                 'value': 'This channel is for team-wide communication and '
+                                          'announcements.All team members are in this channel.',
+                                 'creator': '', 'last_set': 0}, 'previous_names': [], 'num_members': 2}
 
-    def test_should_be_true(self):
-        self.assertTrue(True)
-
-
+    def test_should_create_object_from_dict(self):
+        channel = Channel(**self.channel_dict)
+        self.assertEquals(channel.id, 'C3WBCUECA')
+        self.assertEquals(channel.name, 'general')
+        self.assertEquals(channel.created, 1485362015)
+        self.assertEquals(channel.is_channel, True)
